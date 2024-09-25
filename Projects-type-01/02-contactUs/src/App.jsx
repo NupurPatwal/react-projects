dfa15a794cb4efc4e9afadb044a505aa341fd83e
Nupur Patwal <nupurpatwal@outlook.com>
2024-09-25 12:36:31 +0530

    setting up the second project

---
+++ Projects-type-01/02-contactUs/src/App.jsx
@@ -0,0 +1,11 @@
+// import { useState } from 'react'
+
+function App() {
+
+  return (
+  <>
+  <h1>Hello Contact Us</h1></>
+  )
+}
+
+export default App
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 > Description: Learning projects from YT tutorials.
 
 ## 01 Brand Page
+## 01 contact us page
