c189598298e67fbe39985f961a491100e9b6026f
Nupur Patwal <nupurpatwal@outlook.com>
2024-09-27 18:33:31 +0530

    news-mag started

---
+++ Projects-type-01/news-mag/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar"
+const App = () => {
+  return (
+    <>
+    <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
+
+
+// ba3245a1618541f99ff1cfde929aecad
